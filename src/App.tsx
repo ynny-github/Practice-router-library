4f270d635c35f8024906ac3e57a62a9dde9133cc
Yuya Nagai <ynny.opem@gmail.com>
2021-02-12 13:40:03 +0000

    ネスト route の動作確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Link, Route, Switch, useHistory, useRouteMatch } from "react-router-dom";
+
+function ElementWithNestedRoute() {
+  const {path, url} = useRouteMatch();
+  const history = useHistory();
+
+  const onClick = () => {
+    history.push(`${url}/1-nested`)
+  }
+
+  return (
+    <React.Fragment>
+    <Link to={`${url}/1-nested`}>
+      link
+    </Link>
+    <Switch>
+      <Route exact path={`${path}`}>
+        <button onClick={onClick}>test</button>
+      </Route>
+      <Route path={`${path}/1-nested`}>
+        1 nested
+      </Route>
+    </Switch>
+    </React.Fragment>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Link to="/test"> test</Link>
+        </Route>
+        <Route path="/test">
+          <ElementWithNestedRoute />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
